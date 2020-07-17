0dfae14fd17e575aa0591258b48aa6021a6d3d82
Paul Alcabasa <paulalcabasa@gmail.com>
2020-07-17 09:49:00 +0800

    remove generate

--- resources/frontend/src/views/pages/ViewCoupon.vue
+++ resources/frontend/src/views/pages/ViewCoupon.vue
@@ -16,7 +16,6 @@
       <template v-slot:toolbar>
         <b-button :disabled="disableActions" v-show="approveFlag" @click="approve" size="sm" variant="success">Approve</b-button>
         <b-button :disabled="disableActions" v-show="approveFlag" @click="reject" size="sm" variant="danger" class="ml-2">Reject</b-button>
-        <b-button :disabled="disableActions" v-show="printFlag" @click="print" size="sm" variant="primary" class="ml-2">Generate</b-button>
         <b-button :disabled="disableActions" v-show="isAbletoEdit" @click="edit" size="sm" variant="primary" class="ml-2">Update</b-button>
         <b-button  :disabled="disableActions" @click="resendRequestor" v-if="couponDetails.status_id == 12 || couponDetails.status_id == 3" size="sm" variant="primary" class="ml-2">Resend</b-button>
       
