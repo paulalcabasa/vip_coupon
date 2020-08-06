fdf1a4f959a6edec3bd973608b199b2aa710255d
Paul Alcabasa <paulalcabasa@gmail.com>
2020-08-06 17:50:25 +0800

    remove toast in claim request

--- debug/Approve claim.sql
+++ debug/Approve claim.sql
@@ -1,5 +1,5 @@
 DECLARE 
-    v_coupon_id number := :p_coupon_id;
+    v_claim_header_id number := :v_claim_header_id;
 
 BEGIN
        
@@ -8,20 +8,20 @@ BEGIN
     SET status = 2,
             mail_sent_flag = 'Y',
             date_mail_sent = SYSDATE
-    WHERE module_reference_id = v_coupon_id
-        AND module_id = 1;
+    WHERE module_reference_id = v_claim_header_id
+        AND module_id = 2;
     
     
     -- UPDATE STATUS OF COUPON 
-    UPDATE  ipc.ipc_vpc_coupons
+    UPDATE  ipc.ipc_vpc_claim_headers
     SET status = 2,
             current_approval_hierarchy = (
                 SELECT max(hierarchy)
                 FROM ipc.ipc_vpc_approval
-                WHERE module_reference_id = v_coupon_id
-                    AND module_id = 1
+                WHERE module_reference_id = v_claim_header_id
+                    AND module_id = 2
             )
-    WHERE id = v_coupon_id;
+    WHERE id = v_claim_header_id;
     
     
     COMMIT;
