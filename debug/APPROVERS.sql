0a5c85169a1614f51e092b338bd5314de68ac72a
(MIS)-John Paul Alcabasa <paul-alcabasa@isuzuphil.com>
2021-02-02 17:40:41 +0800

    change timezone

--- debug/APPROVERS.sql
+++ debug/APPROVERS.sql
@@ -8,12 +8,35 @@ FROM ipc.ipc_vpc_approvers approver
     left join ipc_vpc_users_v users
         on users.user_id = approver.approver_user_id
         and users.user_source_id = approver.approver_source_id
-where approver.coupon_type_id = 2
+where approver.coupon_type_id = 1
     and approver.module_id = 2
-    and approver.user_type_id = 47
-  --  and approver.vehicle_type = 'CV'
+    and approver.user_type_id = 45
+   and approver.vehicle_type = 'LCV'
 order by approver.hierarchy;
-1483 - 1
+
+SELECT *
+FROM IPC.IPC_VPC_CLAIM_HEADERS
+WHERE ID = 34;
+SELECT A.ROWID,
+        A.*
+FROM IPC.IPC_VPC_APPROVAL A
+WHERE MODULE_REFERENCE_ID = 34
+AND MODULE_ID = 2;
+
+
+select *
+from ipc.ipc_vpc_approvers;
+
+select *
+from ipc.ipc_vpc_modules; 
+
+select *
+from ipc_portal.systems;
+
+select *
+from ipc_portal.system_user_types
+where system_id = 12;
+
 select *
 from ipc_vpc_users_v;
 
